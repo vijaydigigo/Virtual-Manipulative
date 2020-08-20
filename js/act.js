8f905433ecf7c1305bb761e08be4c68cbbd2a57b
vijay <vijayd@digixl.com>
2020-08-20 22:08:44 +0530

    "update1"

---
+++ js/act.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+	 $("body").prepend('<canvas id="canvas" width='+window.innerWidth+' height='+window.innerHeight+'></canvas>')
+	 
+	//$("#canvas").css({"height":window.innerHeight})
+	$(".actContainer").css({"height":window.innerHeight})
+	 
+
+
+
+
+
+
+
+});
